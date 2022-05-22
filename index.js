333004c5ba257a5baa2b353b024c8f571a69844f
thakurSaad <thakursaad613@gmail.com>
2022-05-22 14:31:06 +0600

    sensitive info protected with env

--- index.js
+++ index.js
@@ -8,6 +8,22 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.rub0a.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+async function run() {
+  try {
+    await client.connect();
+    console.log("mongo eating steel");
+  } finally {
+  }
+}
+run().catch(console.dir);
+
 app.get("/", (req, res) => {
   res.send("Iron worker continues to hit");
 });
