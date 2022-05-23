7f76d3ada320e79ea72c5c330b1ab0f2a1dc8056
thakurSaad <thakursaad613@gmail.com>
2022-05-23 23:49:15 +0600

    PUT api to make user admin

--- index.js
+++ index.js
@@ -118,6 +118,17 @@ async function run() {
       res.send(result);
     });
 
+    // make admin
+    app.put("/users/admin/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: ObjectId(id) };
+      const updateDoc = {
+        $set: { role: "admin" },
+      };
+      const result = await usersCollection.updateOne(filter, updateDoc);
+      res.send(result);
+    });
+
     //----------------------------  DELETE api ---------------------------- //
 
     // delete my order
