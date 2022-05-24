992333ecdaeb71d6a1baf37b37474604ffcdccda
thakurSaad <thakursaad613@gmail.com>
2022-05-24 10:32:03 +0600

    accessToken issued

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const jwt = require("jsonwebtoken");
 require("dotenv").config();
 
 const app = express();
@@ -147,7 +148,14 @@ async function run() {
         updateDoc,
         options
       );
-      res.send(result);
+      const token = jwt.sign(
+        { email: email },
+        process.env.ACCESS_TOKEN_SECRET,
+        {
+          expiresIn: "1h",
+        }
+      );
+      res.send({ result, token });
     });
 
     // make admin
